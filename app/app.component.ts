5aebf933028d6c4fe08599d98f4315668ccf750a
Olivier Chantereau <o.chantereau@itecor.com>
2016-03-01 12:24:30 +0100

    no message

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by o.chantereau on 01.03.2016.
+ */
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Hello World</h1>'
+})
+export class AppComponent {
+}
---
+++ app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by o.chantereau on 01.03.2016.
+ */
+import {bootstrap}    from 'angular2/platform/browser'
+import {AppComponent} from './app.component'
+
+bootstrap(AppComponent);
