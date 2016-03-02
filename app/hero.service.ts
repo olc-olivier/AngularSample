651ba78bf3e90a8f562a317885b022ec96016206
Olivier Chantereau <o.chantereau@itecor.com>
2016-03-02 15:38:21 +0100

    no message

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,3 +1,22 @@
 /**
  * Created by o.chantereau on 02.03.2016.
  */
+import {Hero} from './hero';
+import {Injectable} from 'angular2/core';
+import {HEROES} from "./mock-heroes";
+
+@Injectable()
+export class HeroService {
+
+    getHeroes() {
+        return Promise.resolve(HEROES);
+    }
+
+    // See the "Take it slow" appendix
+    getHeroesSlowly() {
+        return new Promise<Hero[]>(resolve =>
+            setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
+        );
+    }
+
+}
